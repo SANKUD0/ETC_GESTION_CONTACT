bb9dcab5b2357b9a2509a0daadab168180d7cb36
SANKUD0 <tacofauche@gmail.com>
2024-11-06 16:26:50 -0500

    Ajout du frontend

---
+++ Frontend/ETC_GESTION_CONTACT/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using ETC_GESTION_CONTACT.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ETC_GESTION_CONTACT.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CestQuoi()
+        {
+            return View();
+        }
+        public IActionResult Usage()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Frontend/ETC_GESTION_CONTACT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ETC_GESTION_CONTACT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
