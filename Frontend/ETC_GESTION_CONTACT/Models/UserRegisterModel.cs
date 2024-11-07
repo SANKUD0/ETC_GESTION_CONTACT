183e7bdd56fbe89f6711eb3a34a317fdc7373560
S4NKUDO <163693177+SANKUD0@users.noreply.github.com>
2024-11-07 16:46:28 -0500

    Ajout de la classe UserRegisterModel.cs

--- Frontend/ETC_GESTION_CONTACT/Models/UserRegisterModel.cs
+++ Frontend/ETC_GESTION_CONTACT/Models/UserRegisterModel.cs
@@ -2,5 +2,17 @@
 {
     public class UserRegisterModel
     {
+        required
+        public string FirstName { get; set; }
+        required
+        public string LastName { get; set; }
+        required
+        public string PhoneNumber { get; set; }
+        required
+        public string Email { get; set; }
+        required
+        public string Password { get; set; }
+        required
+        public string ConfirmPassword { get; set; }
     }
 }
