4235e2bf42b91f78479fdcad0f97b21a13ee44cf
SANKUD0 <tacofauche@gmail.com>
2024-11-07 20:02:46 -0500

    Nouveau controlleur

---
+++ Frontend/ETC_GESTION_CONTACT/Controllers/GestionContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ETC_GESTION_CONTACT.Controllers
+{
+    public class GestionContactController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
