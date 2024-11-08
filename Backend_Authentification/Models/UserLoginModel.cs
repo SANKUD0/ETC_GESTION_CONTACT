0999368bdbdbb1c019ec5ae5cd258c490a21f038
S4NKUDO <163693177+SANKUD0@users.noreply.github.com>
2024-11-08 00:51:56 -0500

    Ajout de deux classe. Un pour la création de compte, un pour la connexion du compte

---
+++ Backend_Authentification/Models/UserLoginModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend_Authentification.Models
+{
+    public class UserLoginModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
