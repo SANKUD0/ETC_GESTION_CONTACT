212188c29d008f34894e81241f8a7bd6c68703df
S4NKUDO <163693177+SANKUD0@users.noreply.github.com>
2024-11-07 21:12:57 -0500

    Configuration de l'envois de la requête au service backend de l'authentification

--- Frontend/ETC_GESTION_CONTACT/Controllers/AccountController.cs
+++ Frontend/ETC_GESTION_CONTACT/Controllers/AccountController.cs
@@ -5,20 +5,53 @@ namespace ETC_GESTION_CONTACT.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly HttpClient _httpClient;
+        public AccountController()
+        {
+            // Configure l'URL de base pour pointer vers le backend
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:5148");
+        }
+
+        // Affiche la page de connexion
         public IActionResult Login()
         {
             return View();
         }
+        // Gère la soumission du formulaire de connexion
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginModel model)
+        {
+            // Envoie la requête POST au backend avec les informations de connexion
+            var responde = await _httpClient.PostAsJsonAsync("/api/auth/login", model);
+            // Si la connexion est réussie, redirige vers la page d'accueil
+            if (responde.IsSuccessStatusCode)
+                return RedirectToAction("Index", "Home");
+            else
+            {
+                // Si la connexion échoue, affiche un message d'erreur
+                ViewBag.Error = "Email ou mot de passe incorrect";
+                return View(model);
+            }
+        }
 
+        // Affiche la page d'inscription
         public IActionResult Register()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Login(UserLoginModel model)
+        public async Task<IActionResult> Register(UserRegisterModel model)
         {
-            return RedirectToAction("Home", "GestionContact");
+             var response = await _httpClient.PostAsJsonAsync("/api/auth/register", model);
+            if (response.IsSuccessStatusCode)
+                return RedirectToAction("Login");
+            else
+            {
+                ViewBag.Error = "Erreur lors de la création du compte";
+                return View(model);
+            }
         }
     }
 }
